3dbc92c4273f86f6480ef31aadf24222a383d96f
Ruchikasaini429 <ruchikaprofile@gmail.com>
2026-06-12 15:48:48 +0000

    Added docker demo projects

---
+++ pandas-demo/pandas-demo.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Age': [25, 30, 35, 40, 45],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+}   
+df = pd.DataFrame(data)
+print(df)
